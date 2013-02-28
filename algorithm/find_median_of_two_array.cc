6b9fccc59d0e337d9bba7b01d337816317a0e4ae
muye <530708976@qq.com>
2013-02-28 09:26:44 +0800

    optimize the interface

--- algorithm/find_median_of_two_array.cc
+++ algorithm/find_median_of_two_array.cc
@@ -1,7 +1,7 @@
 // Copyright (c) 2013 default
 // Author: Muye (muyepiaozhou@gmail.com)
 // 从两个排好序的数组中寻找中位数.
-// 这个程序时间复杂度为O(log(max(m,n)))
+// 这个程序时间复杂度为O(log(min(m,n)))
 
 #include <iostream>
 #include <vector>
@@ -12,30 +12,30 @@
 #include <cassert>
 using namespace std;
 
-void print(vector<int> &v, int l, int r, ostream& out) {
+void print(vector<int> &v, int l, int r) {
     for(;l <= r; ++l) {
-        out << v[l] << " ";
+        cout << v[l] << " ";
     }
-    out << endl;
+    cout << endl;
 }
 
-int find_median(vector<int> &a, int la, int ra, vector<int> &b, int lb, int rb, const int &key, ostream& out) {
+int find_median(vector<int> &a, int la, int ra, vector<int> &b, int lb, int rb) {
     if(la > ra || lb > rb) {
         return -1;
     }
     int ma = la + ((ra - la) >> 1);
     int mb = lb + ((rb - lb) >> 1);
 #ifdef __DEBUG__
-    out << "A[" << la << ":" << ra << "] ";
-    print(a, la, ra, out);
-    out << "B[" << lb << ":" << rb << "] ";
-    print(b, lb, rb, out);
-    out << "A[" << ma << "] = " << a[ma] << endl;
-    out << "B[" << mb << "] = " << b[mb] << endl;
+    cout << "A[" << la << ":" << ra << "] ";
+    print(a, la, ra);
+    cout << "B[" << lb << ":" << rb << "] ";
+    print(b, lb, rb);
+    cout << "A[" << ma << "] = " << a[ma] << endl;
+    cout << "B[" << mb << "] = " << b[mb] << endl;
 #endif
     if(la == ra || lb == rb) {
         int z;
-        if(ma + mb + 1 > key) {
+        if(ma + mb + 1 > (a.size()+b.size()-1)/2) {
             z = min(a[ma], b[mb]);
             z = max(z, (ma == 0 ? INT_MIN : a[ma-1]));
             z = max(z, (mb == 0 ? INT_MIN : b[mb-1]));
@@ -49,16 +49,16 @@ int find_median(vector<int> &a, int la, int ra, vector<int> &b, int lb, int rb, 
     if(a[ma] < b[mb]) {
         int step = min(ra-ma, mb-lb);
         if(step%2 == 0) {
-            return find_median(a, ma, ma+step, b, mb-step, mb, key, out);
+            return find_median(a, ma, ma+step, b, mb-step, mb);
         } else {
-            return find_median(a, ma+1, ma+step, b, mb-step, mb-1, key, out);
+            return find_median(a, ma+1, ma+step, b, mb-step, mb-1);
         }
     } else if(a[ma] > b[mb]) {
         int step = min(ma-la, rb-mb);
         if(step%2 == 0) {
-            return find_median(a, ma-step, ma, b, mb, mb+step, key, out);
+            return find_median(a, ma-step, ma, b, mb, mb+step);
         } else {
-            return find_median(a, ma-step, ma-1, b, mb+1, mb+step, key, out);
+            return find_median(a, ma-step, ma-1, b, mb+1, mb+step);
         }
     } else {
         return a[ma];
@@ -91,15 +91,15 @@ int main(int argc, char **argv) {
         sort(C.begin(), C.end());
         key = (C.size()-1) >> 1;
 #ifndef __DEBUG__
-        assert(find_median(A, 0, lena-1, B, 0, lenb-1, key, cout) == C[key]);
+        assert(find_median(A, 0, lena-1, B, 0, lenb-1) == C[key]);
 #else
-        if(find_median(A, 0, lena-1, B, 0, lenb-1, key, cout) != C[key]) {
+        if(find_median(A, 0, lena-1, B, 0, lenb-1) != C[key]) {
             cerr << "=================== Error ===================" << endl;
             cerr << "C[0:" << lenb+lena-1 << "] ";
             print(C, 0, lenb+lena-1, cerr);
             cerr << "lena = " << lena << endl;
             cerr << "lenb = " << lenb << endl;
-            cerr << "Wrong : " << find_median(A, 0, lena-1, B, 0, lenb-1, key, cerr) << endl;
+            cerr << "Wrong : " << find_median(A, 0, lena-1, B, 0, lenb-1) << endl;
             cerr << "Right : " << C[key] << endl;
             cerr << "=================== ***** ===================" << endl;
             exit(-1);
